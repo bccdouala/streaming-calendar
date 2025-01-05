cf704d13b1ca0adc3c9d5519dca8df0bf254a779
Johan Bell <belljohan3@gmail.com>
2025-01-05 14:05:28 +0100

    add readme

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,11 +8,7 @@ app.use(cors());
 // Parse the iCal feed
 app.get("/schedule", async (req, res) => {
   try {
-    // const url =
-    //   "https://widgets.bcc.no/ical-69db4c9e2fa0f3e9/52030/Portal-Calendar.ics"; // Replace with your iCal URL
-
-    const url =
-      "https://outlook.live.com/owa/calendar/3ce38da0-475e-4471-8c87-faa2514951ef/4edc3b10-d2d0-4ddf-a9b5-a501a71c1e91/cid-88C72C3D78956F32/calendar.ics";
+    const url = "[Your iCal URL here]";
     const data = await ical.async.fromURL(url);
 
     const events = Object.values(data)
